a5a9fbf8f2088f55c47beed3e62dfd81f3437985
GonzaloSaad <saad.gonzalo.ale@gmail.com>
2019-06-28 00:20:18 -0300

    Configurando Serverless

--- src/main/java/com/universy/auth/util/exceptions/CorruptCognitoEnvironmentSetUpException.java
+++ src/main/java/com/universy/auth/util/exceptions/CorruptCognitoEnvironmentSetUpException.java
@@ -1,4 +1,7 @@
 package com.universy.auth.util.exceptions;
 
 public class CorruptCognitoEnvironmentSetUpException extends IllegalStateException {
+    public CorruptCognitoEnvironmentSetUpException() {
+        super("Environment for Cognito is not properly set.");
+    }
 }
